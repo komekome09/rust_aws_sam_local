1d95462634804fb45a109edb229c6efeca43ef83
komekome09 <fanmaskome@gmail.com>
2020-04-15 01:03:34 +0900

    add error process

--- build_and_run_sam.sh
+++ build_and_run_sam.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 cd $HOME/rust/rust_aws_sam_local/
+
 docker run --rm -it -v "$(pwd)":/home/rust/src \
 -v "$(pwd)/cargo-git":/home/rust/.cargo/git \
 -v "$(pwd)/cargo-registry":/home/rust/.cargo/registry \
 -v "$(pwd)/target":/home/rust/src/target \
 ekidd/rust-musl-builder cargo build --release
+
+if [ $? -ne 0 ]; then
+    exit
+fi
+
 cd $HOME/rust/rust_aws_sam_local/sam
+
 zip -j rust.zip ../target/x86_64-unknown-linux-musl/release/bootstrap
 sam local invoke -e test.json
