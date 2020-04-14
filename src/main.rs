b3de3220898f7ce14d1fdd95f596aac559017cb1
komekome09 <fanmaskome@gmail.com>
2020-04-15 01:39:55 +0900

    remove sam local setting

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@ extern crate serde_derive;
 #[macro_use]
 extern crate log;
 extern crate simple_logger;
-extern crate rewqest;
+extern crate reqwest;
 
 use lambda::error::HandlerError;
 use std::error::Error;
 use simple_error::bail;
-use std::net;
-use std::io::{Write,Read};
+use std::env;
 
 #[derive(Deserialize, Clone)]
 struct CustomEvent {
@@ -31,18 +30,56 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(Serialize, Deserialize)]
+struct AuthToken {
+    token_type: String,
+    expires_in: u32,
+    ext_expires_in: u32,
+    access_token: String,
+}
+
 fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
     if e.first_name == "" {
         error!("Empty first name in request {}", c.aws_request_id);
         bail!("Empty first name");
     }
 
-    let body = reqwest::get("https://www.dropbox.com/oauth2/authorize")
-                        .await?
-                        .text()
-                        .await?;
+    let client_id = match env::var("CLIENT_ID") {
+        Ok(n) => n,
+        Err(_) => bail!("CLIENT_ID not found")
+    };
+    let secret_key = match env::var("CLIENT_SECRET") {
+        Ok(n) => n,
+        Err(_) => bail!("CLIENT_SECRET not found")
+    };
+    let tenant_id = match env::var("TENANT_ID") {
+        Ok(n) => n,
+        Err(_) => bail!("TENANT_ID not found")
+    };
+
+    let params = [("client_id", client_id.as_str()),
+                    ("scope", "https://graph.microsoft.com/.default"),
+                    ("client_secret", secret_key.as_str()),
+                    ("grant_type", "client_credentials")];
+    let client = reqwest::blocking::Client::new();
+    let url = format!("https://login.microsoftonline.com/{}/oauth2/v2.0/token", tenant_id);
+    let body_json = client.post(&url)
+                    .header(reqwest::header::CONTENT_TYPE, "application/x-www-form-urlencoded")
+                    .form(&params)
+                    .send()
+                    .unwrap()
+                    .text()
+                    .unwrap();
+
+    let json: AuthToken = match serde_json::from_str(&body_json) {
+        Ok(n) => n,
+        Err(n) => {
+            error!("Error: {:?}", n);
+            bail!("Fail get token");
+        }
+    };
 
     Ok(CustomOutput {
-        message: format!("Hello, {}!\n{:?}", e.first_name, body),
+        message: format!("{}", json.access_token),
     })
 }
